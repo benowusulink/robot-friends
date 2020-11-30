99d682018d4fc6a1e468da75164581ed327693a6
benzotest <benzo120test@outlook.com>
2020-11-30 23:10:05 +0000

    finished

---
+++ docs/precache-manifest.3efc7ecc293407d36db9dd231b4ab734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843c678780489ef741489297219c5217",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "195a0b7273d11a76f0db",
+    "url": "/robot-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d63055ec983ac3b9d189",
+    "url": "/robot-friends/static/css/main.fbc2f762.chunk.css"
+  },
+  {
+    "revision": "195a0b7273d11a76f0db",
+    "url": "/robot-friends/static/js/2.d3bdc170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robot-friends/static/js/2.d3bdc170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63055ec983ac3b9d189",
+    "url": "/robot-friends/static/js/main.92844adf.chunk.js"
+  },
+  {
+    "revision": "5d9488be1d91a049f9d4",
+    "url": "/robot-friends/static/js/runtime-main.9c2bceb8.js"
+  }
+]);
